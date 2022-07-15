2f2b6dbde22f4ea80e10cf2f2fe92e2c96321242
Olesya <H_lesya00@mail.ru>
2022-07-15 19:28:41 +0300

    W_2.2

---
+++ W_2.2/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
+*/
+
+
+string str = 645.ToString(); 
+Console.WriteLine(str[2]); 
+string strF = 78.ToString(); 
+Console.WriteLine("There is no third digit"); 
+string strS = 32679.ToString(); 
+Console.WriteLine(strS[2]);
+
