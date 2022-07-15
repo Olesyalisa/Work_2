c582c77e0c2eda6fc9fc5177463acabd09e5f253
Olesya <H_lesya00@mail.ru>
2022-07-15 19:29:06 +0300

    W_2.3

---
+++ W2.3/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+DniNedeli(6); 
+DniNedeli(7); 
+DniNedeli(1); 
+ 
+void DniNedeli (int num) 
+{ 
+    if (num == 6 || num == 7) 
+    { 
+        Console.WriteLine(num + " :weekend"); 
+    } 
+    else if (num == 1) 
+    { 
+        Console.WriteLine(num + " :not a day off"); 
+    } 
+}
+
